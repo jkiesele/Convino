b8967a884fe8fb59d4ca7076ee5e0c46966eaee2
jkiesele <jkiesele@cern.ch>
2017-01-26 15:23:24 +0100

    make osx script work better

--- osx_env.sh
+++ osx_env.sh
@@ -1,4 +1,12 @@
+if [[ "${PWD##*/}" != "Convino" ]]; 
+then 
+echo Please source the script from the Convino base directory
+
+else
+
 BASEDIR=`pwd`
 
 export DYLD_LIBRARY_PATH=$BASEDIR:$DYLD_LIBRARY_PATH
 export PATH=$BASEDIR:$PATH
+
+fi
