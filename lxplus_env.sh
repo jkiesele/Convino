4fb27baba90cff725b6f63222df3c2eeeeac0860
CoralieNeubueser <jkiesele@cern.ch>
2019-03-27 18:33:15 +0100

    adapted env

--- lxplus_env.sh
+++ lxplus_env.sh
@@ -2,6 +2,6 @@
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "${SCRIPT}")
 
-source /cvmfs/sft.cern.ch/lcg/views/ROOT-latest/x86_64-slc6-gcc49-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-slc6-gcc62-opt/setup.sh
 
 export PATH=$BASEDIR:$PATH
