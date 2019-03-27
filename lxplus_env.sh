108ae8770a5c4c89cdb65d277bf2ee2a2946f0fd
CoralieNeubueser <jkiesele@cern.ch>
2019-03-27 18:09:06 +0100

    lxplus environment file

--- lxplus_env.sh
+++ lxplus_env.sh
@@ -2,11 +2,6 @@
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "${SCRIPT}")
 
-. /afs/cern.ch/sw/lcg/external/gcc/5.3/x86_64-slc6-gcc53-opt/setup.sh
-cd /afs/cern.ch/sw/lcg/app/releases/ROOT/6.06.06/x86_64-slc6-gcc49-opt/root/
-. bin/thisroot.sh
-cd -
+source /cvmfs/sft.cern.ch/lcg/views/ROOT-latest/x86_64-slc6-gcc49-opt/setup.sh
 
-
-export LD_LIBRARY_PATH=$BASEDIR:$BASEDIR/armadillo/usr/local/lib64:$LD_LIBRARY_PATH
 export PATH=$BASEDIR:$PATH
