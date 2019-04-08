5a3470b14ffa50fdf08b658a185c899f50085590
jkiesele <jan.kieseler@cern.ch>
2019-04-08 15:06:30 +0200

    just if warning removal

--- src/simpleFitter.cpp
+++ src/simpleFitter.cpp
@@ -256,6 +256,7 @@ void simpleFitter::fit(){
 
     if(printlevel>0)
         std::cout << "simpleFitter::fit(): minimisation done, errors done, filling info" << std::endl;
+
 	chi2min_=pminimizer_->MinValue();
 	std::vector<double> dummy;
 	dummy.resize(paras_.size(),0);
